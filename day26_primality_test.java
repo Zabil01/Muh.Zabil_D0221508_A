0dda6efc30ac96adeb74dc9768372766fd7b61e6
Muh Zabil <99641448+Zabil01@users.noreply.github.com>
2022-03-19 18:02:31 +0800

    Add files via upload

---
+++ day26_primality_test.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution { 
+    //Write your code here
+
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      BigInteger n = in.nextBigInteger();
+      in.close();
+      System.out.println( n.isProbablePrime(1000) ? "prime" : "not prime" );
+   }
+}
